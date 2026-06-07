e2ab6deb27e85b558f00de0807179dba4f8eae8d
luckyevil300-web <lucky.evil300@gmail.com>
2026-06-07 13:14:35 -0400

    Update GodotMetadataPlugin.java

--- src/main/java/com/musicplayer/godot/GodotMetadataPlugin.java
+++ src/main/java/com/musicplayer/godot/GodotMetadataPlugin.java
@@ -16,7 +16,7 @@ import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.audio.AudioHeader;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
-import org.jaudiotagger.tag.datatype.Artwork;
+import org.jaudiotagger.tag.images.Artwork;
 
 import java.io.File;
 import java.io.FileOutputStream;
